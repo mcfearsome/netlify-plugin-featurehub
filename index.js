b1698052dac3b07b286fa8bdbd1e1af5c9ad1e8f
Jesse McPherson <jesse@mcfearsome.dev>
2021-08-20 11:36:59 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import {
+  EdgeFeatureHubConfig,
+  ClientContext,
+  Readyness,
+} from 'featurehub-javascript-client-sdk';
+import fetch from 'node-fetch';
+
+if (!globalThis.fetch) {
+	globalThis.fetch = fetch;
+}
+
+const buildEnvRe = /^build_env/i;
+
+
+function _buildRequest(edgeUrl, apiKey, userkey, branch) {
+  const url = `${edgeUrl}?sdkUrl=${apiKey}`;
+  let options = {
+    method: 'GET',
+    headers: {
+      'x-featurehub': `userkey=${userkey}&branch=${branch}`,
+    },
+  };
+  return [url, options];
+}
+
+module.exports = {
+  onPreBuild: async function ({ netlifyConfig, inputs }) {
+    const buildEnvRe = new RegExp(`^${inputs.targetPrefix}`, 'i');
+    const prefixReplace = inputs.prefixReplace;
+    const _req = _buildRequest(inputs.edgeUrl, inputs.sdkUrl, inputs.userkey, netlifyConfig.branch)
+    const response = await fetch(_req[0], _req[1]);
+    const data = await response.json();
+    data.forEach(function(item) {
+      item.features.forEach(function(feature) {
+        if (!(buildEnvRe.test(feature.key)) || !(value in feature)) {
+          return
+        }
+        let env_name = feature.key.replace(buildEnvRe, prefixReplace);
+        process.env[env_name.toUpperCase()] = feature.value;
+      });
+    });
+  },
+}
